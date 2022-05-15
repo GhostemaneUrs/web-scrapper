0e41746489b4f5f47de1fdb6492ff6d7bcb9c1c7
Andres Jaramillo <JaramilloG1999@outlook.com>
2022-05-14 19:56:08 -0500

    feat: implement logic consult and form design

--- src/page/Client/ConsultClient/index.jsx
+++ src/page/Client/ConsultClient/index.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import Form from '../../../components/Form'
 
 const index = () => {
   return (
-    <div>index consult</div>
+    <>
+      <div>
+        <h1 className='text-4xl text-center uppercase font-bold text-indigo-900 mb-10'>
+          Validador de derechos
+        </h1>
+        <div className='container-800'>
+          <Form />
+        </div>
+      </div>
+    </>
   )
 }
 
