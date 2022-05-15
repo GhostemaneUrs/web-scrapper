c9a03657007112bfd533d1c7cdf3751468d8d370
Andres Jaramillo <JaramilloG1999@outlook.com>
2022-05-14 19:55:16 -0500

    feat: update structure

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -10,96 +10,101 @@ const index = () => {
     doc: Yup.string().required('Ingresa tu número de documento'),
   })
 
+  const [users, setUser] = useState([])
   const [validateUser, setValidateUser] = useState({
+    doc: '',
     entity: '',
     docType: '',
-    doc: '',
   })
 
+  const handleSubmit = values => {
+    setValidateUser(values)
+    setUser([...users, values])
+  }
+
   return (
-    <div className='md:w-1/2 lg:w-3/5 xl:w-2/5 mx-5 mb-5'>
-      <Formik
-        initialValues={validateUser}
-        validationSchema={schemaValidateUser}
-        onSubmit={(values, { resetForm }) => {
-          resetForm()
-          setValidateUser(values)
-        }}
-      >
-        {({ errors, touched }) => (
-          <Form
-            className='bg-white shadow-md rounded-xl py-10 px-5'
-            autoComplete='off'
-          >
-            <div className='mb-5'>
-              <label
-                className='text-gray-800 uppercase font-bold'
-                htmlFor='name'
-              >
-                Entidad
-              </label>
-              <Field
-                as='select'
-                name='entity'
-                className='block w-full p-2 bg-gray-100 outline-none'
-              >
-                <option defaultChecked value='none'>
-                  -Seleccione-
-                </option>
-                <option value='red'>Red</option>
-                <option value='green'>Green</option>
-                <option value='blue'>Blue</option>
-              </Field>
-              {errors.entity && touched.entity ? (
-                <Alert>{errors.entity}</Alert>
-              ) : null}
-            </div>
-            <div className='mb-5'>
-              <label
-                htmlFor='docType'
-                className='block text-gray-700 uppercase font-bold'
-              >
-                Tipo de identificación
-              </label>
-              <Field
-                as='select'
-                name='docType'
-                className='block w-full p-2 bg-gray-100 outline-none'
-              >
-                <option defaultChecked value='none'>
-                  -Seleccione-
-                </option>
-                <option value='red'>Red</option>
-                <option value='green'>Green</option>
-                <option value='blue'>Blue</option>
-              </Field>
-              {errors.docType && touched.docType ? (
-                <Alert>{errors.docType}</Alert>
-              ) : null}
-            </div>
-            <div className='mb-5'>
-              <label
-                htmlFor='doc'
-                className='block text-gray-700 uppercase font-bold'
-              >
-                Identificación
-              </label>
-              <Field
-                type='text'
-                name='doc'
-                className='block w-full p-2 bg-gray-100 outline-none'
-              />
-              {errors.doc && touched.doc ? <Alert>{errors.doc}</Alert> : null}
-            </div>
-            <input
-              type='submit'
-              className='bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-900 cursor-pointer transition-all'
-              value={'Consultar'}
+    <Formik
+      initialValues={{
+        doc: '',
+        entity: '',
+        docType: '',
+      }}
+      validationSchema={schemaValidateUser}
+      onSubmit={(values, { resetForm }) => {
+        handleSubmit(values)
+        resetForm()
+      }}
+    >
+      {({ errors, touched }) => (
+        <Form
+          className='bg-white shadow-md rounded-xl py-10 px-5'
+          autoComplete='off'
+        >
+          <div className='mb-5'>
+            <label className='text-gray-800 uppercase font-bold' htmlFor='name'>
+              Entidad
+            </label>
+            <Field
+              as='select'
+              name='entity'
+              className='block w-full p-2 bg-gray-100 outline-none'
+            >
+              <option defaultChecked value='none'>
+                -Seleccione-
+              </option>
+              <option value='red'>Red</option>
+              <option value='green'>Green</option>
+              <option value='blue'>Blue</option>
+            </Field>
+            {errors.entity && touched.entity ? (
+              <Alert>{errors.entity}</Alert>
+            ) : null}
+          </div>
+          <div className='mb-5'>
+            <label
+              htmlFor='docType'
+              className='block text-gray-700 uppercase font-bold'
+            >
+              Tipo de identificación
+            </label>
+            <Field
+              as='select'
+              name='docType'
+              className='block w-full p-2 bg-gray-100 outline-none'
+            >
+              <option defaultChecked value='none'>
+                -Seleccione-
+              </option>
+              <option value='red'>Red</option>
+              <option value='green'>Green</option>
+              <option value='blue'>Blue</option>
+            </Field>
+            {errors.docType && touched.docType ? (
+              <Alert>{errors.docType}</Alert>
+            ) : null}
+          </div>
+          <div className='mb-5'>
+            <label
+              htmlFor='doc'
+              className='block text-gray-700 uppercase font-bold'
+            >
+              Identificación
+            </label>
+            <Field
+              type='text'
+              name='doc'
+              className='block w-full p-2 bg-gray-100 outline-none'
             />
-          </Form>
-        )}
-      </Formik>
-    </div>
+            {errors.doc && touched.doc ? <Alert>{errors.doc}</Alert> : null}
+          </div>
+          <input
+            type='submit'
+            className='bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-900 cursor-pointer transition-all'
+            value={'Consultar'}
+          />
+        </Form>
+      )}
+    </Formik>
   )
 }
 
